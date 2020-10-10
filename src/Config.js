6a2637c9cb46d64a496d9ae0878d713fbcbbb9b5
mahvishfaridi <imahvish96@users.noreply.github.com>
2020-10-10 17:39:08 +0530

    comment and try

--- src/Config.js
+++ src/Config.js
@@ -7,5 +7,3 @@ const API_READ_ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDc4MTA1M2JlZWIzMThiMjA4NjQ2YzYxOWY0MTRiYSIsInN1YiI6IjVmNzcxM2Q1NTMyYWNiMDAzODc2NTUwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yKkOrX6QW2zhqgmN3M8ecSFk9_e8yN2wyNmNqPb1geQ";
 
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=00781053beeb318b208646c619f414ba&query=${this.state.searchTerm}`;
-
-const CREDIT_API = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=00781053beeb318b208646c619f414ba`;
